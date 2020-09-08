f6a3fe907b321741ef9f3bd3db3994962a73a549
大庭秀斗 <shuto.0028.0618@gmail.com>
2020-09-08 15:10:33 +0900

    タスク追加機能です

---
+++ src/components/Table.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="table">
+    <label for=""><input type="radio" name="radio" value="全て" checked>全て</label>
+    <label for=""><input type="radio" name="radio" value="作業中">作業中</label>
+    <label for=""><input type="radio" name="radio" value="完了">完了</label>
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>コメント</th>
+          <th>状態</th>
+        </tr>
+      </thead>
+      <tbody id="todo-body">
+        <tr v-for="todo in todos" v-bind:key="todo.id">
+          <td>{{todo.id}}</td>
+          <td>{{todo.comment}}</td>
+          <td>
+            <button>{{todo.status}}</button>
+          </td>
+          <td>
+              <button>削除</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>新規タスクの追加</h2>
+    <input type="text" ref="comment" />
+    <input type="button" value="追加" v-on:click="addTask" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'Table',
+  data() {
+    return {
+      todos: [],
+      id: 1,
+    };
+  },
+  methods: {
+    addTask() {
+      const comment = this.$refs.comment;
+      this.todos.push({
+        id: this.id++,
+        comment: comment.value,
+        status: '作業中',
+      });
+      comment.value = ''
+    },
+  },
+};
+</script>
+<style>
+    table{
+        margin: 0 auto;
+    }
+</style>
